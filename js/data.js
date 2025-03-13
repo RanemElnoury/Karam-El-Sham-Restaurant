6d462fb6dfb2dd0afb0a5c22b0d0161bf8470695
RanemElnoury <120828846+RanemElnoury@users.noreply.github.com>
2025-03-13 03:41:35 +0200

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,128 @@
+const data = {
+    offers: [
+        { title: "Offer 1", image: "../Assets/1.jpg" },
+        { title: "Offer 2", image: "../Assets/2.jpg" },
+        { title: "Offer 3", image: "../Assets/3.jpg" },
+        { title: "Offer 4", image: "../Assets/4.jpg" },
+        { title: "Offer 5", image: "../Assets/5.jpg" },
+        { title: "Offer 6", image: "../Assets/6.jpg" },
+        { title: "Offer 7", image: "../Assets/7.jpg" },
+        { title: "Offer 8", image: "../Assets/8.jpg" },
+        { title: "Offer 1", image: "../Assets/1.jpg" },
+        { title: "Offer 2", image: "../Assets/2.jpg" },
+        { title: "Offer 3", image: "../Assets/3.jpg" },
+        { title: "Offer 4", image: "../Assets/4.jpg" },
+        { title: "Offer 5", image: "../Assets/5.jpg" },
+        { title: "Offer 6", image: "../Assets/6.jpg" },
+        { title: "Offer 7", image: "../Assets/7.jpg" },
+        { title: "Offer 8", image: "../Assets/8.jpg" },
+    ],
+    menu: {
+        sandwiches: [
+            { id: 1, title: "Maria Chicken Shawrma ", image: "../Assets/mariaChicken.jpg", price: 80 },
+            { id: 2, title: "Maria Meet Shawrma", image: "../Assets/mariaMeet.jpg", price: 120 },
+            { id: 3, title: "Maria  Mix Shawrma ", image: "../Assets/mariaMix.jpg", price: 100 },
+            { id: 4, title: "Chicken soory", image: "../Assets/chickenSory.jpg", price: 70 },
+            { id: 5, title: "Meet soory", image: "../Assets/meetSory.jpg", price: 85 },
+            { id: 6, title: "Chicken French", image: "../Assets/chickenFrench.jpg", price: 70 },
+            { id: 7, title: "Meet French", image: "../Assets/meetFrench.jpg", price: 100 },
+            { id: 8, title: "Crispy French", image: "../Assets/crispyFrench.jpg", price: 80 },
+            { id: 9, title: "Eldynasor", image: "../Assets/eldynasor.jpg", price: 120 },
+            { id: 10, title: "Burgar Elkaram", image: "../Assets/burgerElkaram.jpg", price: 120 },
+            { id: 11, title: "Kaiser Chicken Shawarma", image: "../Assets/kyzrShawrmaChicken.jpg", price: 50 },
+            { id: 12, title: "Kaiser Meet Shawarma", image: "../Assets/kyzrShawrmaMeet.jpg", price: 70 },
+            { id: 13, title: "Kaiser Grilled Chicken", image: "../Assets/kyzarChicken.jpg", price: 60 },
+            { id: 14, title: "Kaisar Crispy", image: "../Assets/kyzrCrispy.jpg", price: 50 },
+            { id: 15, title: "Chicken Shawarma Crepe", image: "../Assets/crepShawrmaChicken.jpg", price: 90 },
+            { id: 16, title: "Meet Shawarma Crepe", image: "../Assets/crepMeet.jpg", price: 100 },
+            { id: 17, title: "Crispy Crepe", image: "../Assets/crepCrispy.jpg", price: 80 },
+            { id: 18, title: "Cheesy Lover Crepe", image: "../Assets/crepCheese.jpg", price: 60 },
+        ],
+        meals: [
+            { id: 19, title: "Chicken Shawrma Araby", image: "../Assets/arabyChicken.jpg", price: 100 },
+            { id: 20, title: "Meet Shawrma Araby", image: "../Assets/arabyMeet.jpg", price: 135 },
+            { id: 21, title: "Mix Shawrma Araby", image: "../Assets/arabyMix.jpg", price: 155 },
+            { id: 22, title: "Chicken Shawrma Fata", image: "../Assets/chickenFata.jpg", price: 70 },
+            { id: 23, title: "Meet Shawrma Fata", image: "../Assets/meetFata.jpg", price: 100 },
+            { id: 24, title: "Mix Shawrma Fata", image: "../Assets/mixFata.jpg", price: 90 },
+            { id: 25, title: "Crispy Meal", image: "../Assets/crispyMeal.jpg", price: 150 },
+            { id: 26, title: "Mix Kofta & Grilled Meal", image: "../Assets/mixKofta&Grilled.jpg", price: 120 },
+            { id: 27, title: "Mix Crispy & Shawrma Meal", image: "../Assets/mixCrispy&Shawrma.jpg", price: 120 },
+            { id: 28, title: "Mix Crispy & Grilled Meal", image: "../Assets/mixCrispy&Grilled.jpg", price: 100 },
+            { id: 29, title: "Mix Crispy & Shawrma Araby Meal", image: "../Assets/mixCrispy&arabyShawrma.jpg", price: 150 },
+            { id: 30, title: "Chicken Kofta Meal", image: "../Assets/chickenKoftaMeal.jpg", price: 100 },
+            { id: 31, title: "Shesh Meal", image: "../Assets/sheshMeal.jpg", price: 120 },
+            { id: 32, title: "Grilled Chicken Meal", image: "../Assets/grilledChickenMeal.jpg", price: 120 },
+        ],
+        salads: [
+            { id: 33, title: "Tomia", image: "../Assets/tomia1.jpg", price: 12 },
+            { id: 34, title: "Spicy Tomia", image: "../Assets/tomiaSpicy.jpeg", price: 15 },
+            { id: 35, title: "Thina", image: "../Assets/tehina.jpeg", price: 20 },
+            { id: 36, title: "Kolo Slo", image: "../Assets/koloslo.jpg", price: 25 },
+            { id: 37, title: "Fries", image: "../Assets/fries.jpg", price: 30 },
+            { id: 38, title: "Cheese Fries", image: "../Assets/chesseFries.jpg", price: 50 },
+            { id: 39, title: "Water", image: "../Assets/water.jpg", price: 10 },
+            { id: 40, title: "Coca Cola", image: "../Assets/cocacola.jpg", price: 20 },
+            { id: 41, title: "Sprite", image: "../Assets/sprite.jpg", price: 20 },
+        ],
+        boxes: [
+            { id: 42, title: "9 Peices Fried Chicken", image: "../Assets//box1.jpg", price: 320 },
+            { id: 43, title: "1/5 Kofta & Grilled Chicken", image: "../Assets//box2.jpg", price: 350 },
+            { id: 44, title: "Shesh & Kofta & Grilled Chicken", image: "../Assets//box3.jpg", price: 400 },
+            { id: 45, title: "2 Meat & 2 Chicken Kaisar", image: "../Assets//box4.jpg", price: 220 },
+            { id: 46, title: "9 Peices Fried & 2 Shawrma", image: "../Assets//box5.jpg", price: 450 },
+            { id: 47, title: "5 Meat & 5 Chicken Sory", image: "../Assets//box6.jpg", price: 400 },
+            { id: 48, title: "9 Peices Fried & 2 Fata", image: "../Assets//box7.jpg", price: 500 },
+            { id: 49, title: "Fried Chicken & Shawrma Araby", image: "../Assets//box8.jpg", price: 400 },
+            { id: 50, title: "Shesh & Kofta & Grilled Chicken", image: "../Assets//box9.jpg", price: 500 },
+            { id: 51, title: "1/5 Shesh & Shawrma", image: "../Assets//box10.jpg", price: 22 },
+
+        ]
+    },
+    team: [
+        { id: 1, name: "Mohamed Ali", role: "Head Chef", image: "../Assets/team1.jpg" },
+        { id: 2, name: "Omar Ehab", role: "Chef", image: "../Assets/team2.jpg" },
+        { id: 3, name: "Mohamed Yasser", role: "Sous Chef", image: "../Assets/team3.jpg" },
+        { id: 4, name: "Ahmed Khaled", role: "Grill Chef", image: "../Assets/team4.jpg" },
+    ],
+    branches: [
+        {
+            "name": "Downtown",
+            "address": "Talaat Harb Street, intersection with Abdel Khaleq Tharwat Street, near KFC",
+            "location": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3282.649157118315!2d31.24325972465487!3d30.050431174920618!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145841af9cc83b9b%3A0x9ed0a038dadf283a!2z2YPYsdmFINin2YTYtNin2YU!5e1!3m2!1sen!2seg!4v1741818690287!5m2!1sen!2seg"
+        },
+        {
+            "name": "Smouha",
+            "address": "Smouha Roundabout, first Victor Emmanuel Square, Alexandria Governorate",
+            "location": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3243.407871963555!2d29.950869218327487!3d31.214001908233698!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14f5c496c6cafba5%3A0x742bddb1700e7e8c!2z2YPYsdmFINin2YTYtNin2YU!5e1!3m2!1sen!2seg!4v1741819724867!5m2!1sen!2seg"
+        },
+        {
+            "name": "Al Rehab",
+            "address": "Al Rehab, Shop 34, Old Market, Second New Cairo, Cairo Governorate",
+            "location": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d104945.97162130759!2d31.550663827023296!3d30.14345213684468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14581978bb1ff563%3A0x4c5f38d4353ee196!2z2YPYsdmFINin2YTYtNin2YUgLSDZgdix2Lkg2KfZhNix2K3Yp9io!5e1!3m2!1sen!2seg!4v1741819347363!5m2!1sen!2seg"
+        },
+        {
+            "name": "Gamal Abdel Nasser",
+            "address": "Gamal Abdel Nasser, Barka Al-Nasr, First Al-Salam, Cairo Governorate",
+            "location": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4743.628277091444!2d31.3995387407461!3d30.144273984029862!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14581732024eaedb%3A0xb82e121753c3f5d1!2z2YPYsdmFINin2YTYtNin2YU!5e1!3m2!1sen!2seg!4v1741820422211!5m2!1sen!2seg"
+        },
+        {
+            "name": "Al Montazah - Heliopolis",
+            "address": "Roxy Square, Al Montazah, Heliopolis, Cairo Governorate",
+            "location": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d884479.1029760694!2d32.40528152187499!3d30.00825700000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583f4b24a481bb%3A0xb20709e98f2b4a8d!2z2YPYsdmFINin2YTYtNin2YUg2YHYsdi5INmF2LXYsSDYp9mE2KzYr9mK2K_YqQ!5e0!3m2!1sen!2seg!4v1741819787492!5m2!1sen!2seg"
+        },
+        {
+            "name": "Maadi - Zahraa Al Maadi",
+            "address": "Maadi Sarayat Al-Gharbeyah, Al-Basateen District, Cairo Governorate",
+            "location": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d105044.77300118552!2d31.39312010273436!3d30.050431200000013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583981b8ab278f%3A0x5bf991d7ccba7dfc!2z2K3Yp9ix2Kkg2KfZhNi02KfZhSBIYXJldCBlbHNoYW0!5e1!3m2!1sen!2seg!4v1741819168725!5m2!1sen!2seg"
+        },
+        {
+            "name": "Faisal",
+            "address": "King Faisal Street, Kafr Tohormos, Omrania District, Giza Governorate",
+            "location": "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.046149371432!2d31.188374024656564!3d30.008256974941805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14584754954bd3b1%3A0xe50b87f6c3d4f5ab!2z2YPYsdmFINin2YTYtNin2YUgLSDZgdix2Lkg2YHZiti12YQ!5e1!3m2!1sen!2seg!4v1741819055721!5m2!1sen!2seg"
+        }
+    ]
+    
+};
+
+export default data;
